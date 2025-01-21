fb213bcfddde45042aa797f9c482a95f229b62ea
Nicola Bicocchi <nbicocchi@unimore.it>
2025-01-21 16:09:13 +0100

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/collections/MultiplyOdd.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/collections/MultiplyOdd.java
@@ -1,5 +1,9 @@
 package com.nbicocchi.exercises.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Write a Java program that performs the following operations on an ArrayList of integers:
  *
@@ -9,4 +13,13 @@ package com.nbicocchi.exercises.collections;
  * Print the modified list.
  */
 public class MultiplyOdd {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10));
+        list.removeAll(List.of(2,4,6,8,10));
+
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i) * 2);
+        }
+        System.out.println(list);
+    }
 }
