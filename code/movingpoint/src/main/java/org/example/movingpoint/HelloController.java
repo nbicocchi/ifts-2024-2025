8596baa0f6fc270de4c11e84dcd9032cc2346723
Nicola Bicocchi <nbicocchi@unimore.it>
2025-02-04 15:00:50 +0100

    update

--- code/movingpoint/src/main/java/org/example/movingpoint/HelloController.java
+++ code/movingpoint/src/main/java/org/example/movingpoint/HelloController.java
@@ -1,5 +1,6 @@
 package org.example.movingpoint;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -8,20 +9,43 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Circle;
 
 public class HelloController {
+    AnimationTimer timer;
+
     @FXML
     private ImageView ship;
+    private Vector2D shipSpeed = new Vector2D(0, 0);
+
+
+    public void initialize() {
+        if (timer != null) {
+            timer.stop();
+        }
+        timer = new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                mainLoop();
+            }
+        };
+        timer.start();
+    }
+
+    private void mainLoop() {
+        ship.setTranslateX(ship.getTranslateX() + shipSpeed.getX());
+        ship.setTranslateY(ship.getTranslateY() + shipSpeed.getY());
+    }
 
     void keyPressed(KeyEvent event) {
         System.out.println(event.getCode());
 
         if (event.getCode() == KeyCode.W) {
-            ship.setTranslateY(ship.getTranslateY() - 5);
+            shipSpeed = new Vector2D(0, -0.1);
         }  else if (event.getCode() == KeyCode.S) {
-            ship.setTranslateY(ship.getTranslateY() + 5);
+            shipSpeed = new Vector2D(0, 0.1);
         } else if (event.getCode() == KeyCode.A) {
-            ship.setTranslateX(ship.getTranslateX() - 5);
+            shipSpeed = new Vector2D(-0.1, 0);
         }  else if (event.getCode() == KeyCode.D) {
-            ship.setTranslateX(ship.getTranslateX() + 5);
+            shipSpeed = new Vector2D(0.1, 0);
         }
     }
 
---
+++ code/movingpoint/src/main/java/org/example/movingpoint/Vector2D.java
@@ -0,0 +1,52 @@
+package org.example.movingpoint;
+
+public class Vector2D {
+    private double x;
+    private double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2D subtract(Vector2D other) {
+        return new Vector2D(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2D scale(double scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    public double dot(Vector2D other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vector2D(%.2f, %.2f)", x, y);
+    }
+}
