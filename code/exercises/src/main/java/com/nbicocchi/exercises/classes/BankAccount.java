3fe08f856ea6ed0d4cccfa1e0560b07d2a05b8bb
Nicola Bicocchi <nbicocchi@unimore.it>
2025-02-04 15:05:27 +0100

    update

---
+++ code/exercises/src/main/java/com/nbicocchi/exercises/classes/BankAccount.java
@@ -0,0 +1,9 @@
+package com.nbicocchi.exercises.classes;
+
+// il conto ha:
+// iban, intestatario, saldo, tasso di interesse
+// offre le seguenti funzionalità:
+// inserire/prelevare denaro
+// fare bonifici verso altri conti
+public class BankAccount {
+}
