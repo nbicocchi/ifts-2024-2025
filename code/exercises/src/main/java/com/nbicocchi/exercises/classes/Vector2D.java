34a5d4da9ac8fea8f0b6611ff630bc526e7c28b9
Nicola Bicocchi <nbicocchi@unimore.it>
2025-02-04 14:13:47 +0100

    update

---
+++ code/exercises/src/main/java/com/nbicocchi/exercises/classes/Vector2D.java
@@ -0,0 +1,57 @@
+package com.nbicocchi.exercises.classes;
+
+public class Vector2D {
+    private double x;
+    private double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2D subtract(Vector2D other) {
+        return new Vector2D(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2D scale(double scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    public double dot(Vector2D other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vector2D normalize() {
+        double mag = magnitude();
+        return mag == 0 ? new Vector2D(0, 0) : new Vector2D(x / mag, y / mag);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vector2D(%.2f, %.2f)", x, y);
+    }
+}
