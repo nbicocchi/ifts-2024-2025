9aabb7db0b0ff828b9c8496da2d7e54529d1a592
Nicola Bicocchi <nbicocchi@unimore.it>
2025-02-04 16:01:32 +0100

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/classes/BankAccount.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/classes/BankAccount.java
@@ -6,4 +6,72 @@ package com.nbicocchi.exercises.classes;
 // inserire/prelevare denaro
 // fare bonifici verso altri conti
 public class BankAccount {
+    String IBAN;
+    String owner;
+    double balance;
+    double interestRate;
+
+    public BankAccount(String IBAN, String owner, double balance, double interestRate) {
+        this.IBAN = IBAN;
+        this.owner = owner;
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+
+    public BankAccount(String IBAN, String owner) {
+        this.IBAN = IBAN;
+        this.owner = owner;
+        this.balance = 0.0;
+        this.interestRate = 0.03;
+    }
+
+    public String getIBAN() {
+        return IBAN;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double deposit(double amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount) {
+        balance -= amount;
+        return balance;
+    }
+
+    public double wire(double amount, BankAccount otherAccount) {
+        withdraw(amount);
+        otherAccount.deposit(amount);
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "IBAN='" + IBAN + '\'' +
+                ", owner='" + owner + '\'' +
+                ", balance=" + balance +
+                ", interestRate=" + interestRate +
+                '}';
+    }
 }
